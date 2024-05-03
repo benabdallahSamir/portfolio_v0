f021d725de4a0c8f50ffd40683bdd0c614e21caf
benabdallah samir <samirbenabdallah4002@gmail.com>
2024-05-03 15:40:36 +0200

    hundling css style and add active in item of nav ul

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,34 +5,47 @@ import {
 } from "@fortawesome/free-regular-svg-icons";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 const Navbar = () => {
   const navElement = useRef();
+  const [activeLi, setActiveLi] = useState("home");
   return (
     <nav className="flex-ac" ref={navElement}>
       <h1 onClick={() => navElement.current.classList.toggle("active")}>BA</h1>
       <ul className="flex-ac">
-        <li>
-          <a className="flex-ac" href="#">
+        <li className={`${activeLi === "home" ? "active" : ""}`}>
+          <a className="flex-ac" href="#" onClick={() => setActiveLi("home")}>
             <FontAwesomeIcon icon={faHome} />
             <p>home</p>
           </a>
         </li>
-        <li>
-          <a className="flex-ac" href="/#about">
+        <li className={`${activeLi === "about" ? "active" : ""}`}>
+          <a
+            className="flex-ac"
+            href="/#about"
+            onClick={() => setActiveLi("about")}
+          >
             <FontAwesomeIcon icon={faUser} />
             <p>about me</p>
           </a>
         </li>
-        <li>
-          <a className="flex-ac" href="/#project">
+        <li className={`${activeLi === "project" ? "active" : ""}`}>
+          <a
+            className="flex-ac"
+            href="/#project"
+            onClick={() => setActiveLi("project")}
+          >
             <FontAwesomeIcon icon={faFolder} />
             <p>projects</p>
           </a>
         </li>
-        <li>
-          <a className="flex-ac" href="/#contact">
+        <li className={`${activeLi === "contact" ? "active" : ""}`}>
+          <a
+            className="flex-ac"
+            href="/#contact"
+            onClick={() => setActiveLi("contact")}
+          >
             <FontAwesomeIcon icon={faContactCard} />
             <p>contact</p>
           </a>
